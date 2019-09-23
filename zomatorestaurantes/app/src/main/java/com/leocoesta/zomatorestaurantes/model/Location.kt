ebaff678174dc55b5a295aa426162216f0a8f2cb
Leonardo Costa <coesta@users.noreply.github.com>
2019-09-23 15:54:46 -0300

    Implementação de getLocations parcial

---
+++ zomatorestaurantes/app/src/main/java/com/leocoesta/zomatorestaurantes/model/Location.kt
@@ -0,0 +1,7 @@
+package com.leocoesta.zomatorestaurantes.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class LocationResponse(): Parcelable
--- zomatorestaurantes/app/src/main/java/com/leocoesta/zomatorestaurantes/network/ZomatoApiService.kt
+++ zomatorestaurantes/app/src/main/java/com/leocoesta/zomatorestaurantes/network/ZomatoApiService.kt
@@ -8,6 +8,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 //user-key: dc0b52faed0ea45b0f7bd744330deb38
 
@@ -15,6 +16,16 @@ interface ZomatoApiService {
     @Headers("user-key: dc0b52faed0ea45b0f7bd744330deb38")
     @GET(value = "categories")
     suspend fun getCategories(): CategoryResponse
+
+
+    @Headers("user-key: dc0b52faed0ea45b0f7bd744330deb38")
+    @GET(value = "locations")
+    suspend fun getLocation(
+        @Query(value = "q") query: String,
+        @Query(value = "lat") latitude: Double,
+        @Query(value = "lon") longitude: Double,
+        @Query(value = "count") maxResultsToFetch: String
+    )
 }
 
 private val moshi = Moshi.Builder()
